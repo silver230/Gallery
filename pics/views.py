a42b355f1e693267b76a6538fdf8f3b39b8ff7a1
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-06 18:35:43 +0300

    wriiten a function to see if my nav is acctually showing

--- pics/views.py
+++ pics/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import Http404
 
 # Create your views here.
+def intro(request):
+    return render(request, 'navbar.html')
