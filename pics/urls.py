e67754efb4661b223e0fa430bbd8738694827427
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-07 10:19:04 +0300

    written  ur for date and day

--- pics/urls.py
+++ pics/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns=[
-    url(r'^$',views.intro,name='header'),
+    url(r'^$',views.pics_of_day,name='picsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_pics,name = 'pastPics'),
     
 ]
