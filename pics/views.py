a84fe2ade5f8517fec185b06575bb111efd8bb7c
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-07 10:25:04 +0300

    defined my date and converted it to days of the week also defined a function when the pic was taken

--- pics/views.py
+++ pics/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render,redirect
 from django.http  import Http404
+import datetime as dt
 
 # Create your views here.
-def intro(request):
-    return render(request, 'navbar.html')
+# def intro(request):
+#     return render(request, 'all-pics/gallery.html')
+
+def pics_of_day(request):
+    date = dt.date.today()
+    
+    return render(request, 'all-pics/gallery.html', {"date": date})    
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+def past_days_pics(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_today)
+
+    return render(request, 'all-pics/past-gallery.html',{"date": date})
