096addddd5be59a712d9f8dfef7d71598d956c6b
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-09 20:13:34 +0300

    set url search path

--- pics/urls.py
+++ pics/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns=[
     url(r'^$',views.pics_of_day,name='pics'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_pics,name = 'pastPics'),
+    url(r'^search/', views.search_results, name='search_results'),
     
 ]
