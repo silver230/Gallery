b1710cd2b6214d370b3755ff6e3ac8ca3a06b02c
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-10 08:57:07 +0300

    installed psycopg2-binary to avoid its repetition

--- pics/models.py
+++ pics/models.py
@@ -28,8 +28,8 @@ class Category(models.Model):
     def __str__(self):
        return self.name
     
-    # def save_category(self):
-    #     self.save()
+    def save_category(self):
+        self.save()
 
     # def delete_category(self):
     #     self.delete()
